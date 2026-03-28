07bc9c13dfe01924822ec17154b52f9a9f1a0ead
vmovies404 <84915625+vmovies404@users.noreply.github.com>
2026-03-28 08:46:05 +0530

    Update app.run to use dynamic port from environment
    Modify app.run to use environment variable for port

--- app.py
+++ app.py
@@ -69,4 +69,7 @@ def delete(id):
 
     return redirect(url_for("history"))
 
-app.run(debug=True, port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
